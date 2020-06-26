a28d2009e88fa490a63d917fc23f4e9219d4e5cf
dosullivansoftware <danielosullivansoftware@gmail.com>
2020-06-26 12:14:50 +0100

    Removing BaseEntity to avoid potential snowballing dependency

--- cargo-tracker-build/cargo-tracker-repository/src/main/java/osullivan/daniel/travel_helper/admin/Cargo.java
+++ cargo-tracker-build/cargo-tracker-repository/src/main/java/osullivan/daniel/travel_helper/admin/Cargo.java
@@ -1,17 +1,30 @@
 package osullivan.daniel.travel_helper.admin;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table
-public class Cargo extends BaseEntity {
+public class Cargo {
 
+	@Id
+	@Column
+	private Long id;
 	@OneToOne
 	private Port origin;
 	@OneToOne
 	private Port destination;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public Port getOrigin() {
 		return origin;
--- cargo-tracker-build/cargo-tracker-repository/src/main/java/osullivan/daniel/travel_helper/admin/Port.java
+++ cargo-tracker-build/cargo-tracker-repository/src/main/java/osullivan/daniel/travel_helper/admin/Port.java
@@ -2,14 +2,26 @@ package osullivan.daniel.travel_helper.admin;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table
-public class Port extends BaseEntity {
+public class Port {
 
+	@Id
+	@Column
+	private Long id;
 	@Column
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
